2b4edb59480f7a1752705f3f99f1ddc0e9872c19
ramiis <runger324@gmail.com>
2025-10-30 13:26:01 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='enchant_lang',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Write Python code in Minecraft Enchantment Table language.',
+    author='YOUR NAME',
+    url='https://github.com/YOURUSERNAME/enchant_lang',
+    python_requires='>=3.7',
+)
